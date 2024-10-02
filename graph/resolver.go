db7ab78ed827a8a7342a0b1d062c72cd81a0dceb
Yuki Uchiyama <uchiyama.yuki@moneyforward.co.jp>
2024-10-02 14:42:57 +0900

    Update resolver

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,13 @@
 package graph
 
+import (
+	"gqlgen_tutorial/graph/model"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	todos []*model.Todo
+}
